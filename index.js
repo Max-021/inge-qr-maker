b706c36bc0461f4c88e47c8a310c5e21f8a2c99a
Maximiliano Frega <maxifrega21@gmail.com>
2023-06-26 17:42:08 -0300

    cambios

--- index.js
+++ index.js
@@ -25,37 +25,15 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 	.catch(error => {console.log(error)})
 
 app.use(morgan('tiny'))
-app.use(cors({
-    origin:true,
-    optionsSuccessStatus:200,
-    credentials:true,
-}));
-app.options(
-    '*',
-    cors({
-        origin:true,
-        optionsSuccessStatus:200,
-        credentials: true,
-    })
-)
+app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);                
-//     next()
-// })
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-    });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);                
+    next()
+})
 
 app.use((err, req, res, next) => {
 	handleError(err, res)
