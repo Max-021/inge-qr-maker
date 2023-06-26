fb9f2db8bf4a23822627f90a2c402a5f0a912f7c
Maximiliano Frega <maxifrega21@gmail.com>
2023-06-26 14:44:51 -0300

    cs

--- index.js
+++ index.js
@@ -25,14 +25,11 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 	.catch(error => {console.log(error)})
 
 app.use(morgan('tiny'))
-app.use(cors({
-    credentials: true,
-    origin: true,
-}))
+app.use(cors())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);                
     next()
